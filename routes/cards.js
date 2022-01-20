2a0580821b46ddf0a6a41503f00d638c00c5f778
Luis Marsiglia <luismarsiglia19@gmail.com>
2022-01-19 20:59:58 -0400

    v1.0.2: added previous patches card balances

--- routes/cards.js
+++ routes/cards.js
@@ -1,16 +1,31 @@
 const express = require('express')
 const router = express.Router()
-const axios = require('axios')
 const cache = require('../middlewares/cache')
+const { fixCardsFormat, patchFiles, patchIds } = require('../utils')
 
-// TODO: show cards data of previous seasons
+const availablePatches = patchIds.join(', ')
+
 router.get('/', cache(300), async (req, res, next) => {
-  const url =
-    'https://storage.googleapis.com/axie-cdn/game/cards/card-abilities.json'
+  res.status(400).json({
+    error: `You should choose the season patch balance you want to see in path /cards/{patchId}. Available patches: ${availablePatches}`
+  })
+})
+
+router.get('/:patchId', cache(300), async (req, res, next) => {
+  const { patchId } = req.params
+  const { format } = req.query
+
   try {
-    const response = await axios.get(url)
-    // TODO: fix json data
-    res.json(response.data)
+    if (patchFiles[patchId]) {
+      if (format === 'original') res.json(patchFiles[patchId])
+      else res.json(fixCardsFormat(patchFiles[patchId]))
+    } else {
+      res
+        .status(400)
+        .json({
+          error: `Unknown patchId. Available patches: ${availablePatches}`
+        })
+    }
   } catch (error) {
     next(error)
   }

2cdf3f43a9158e9da2981c6501c7eaedd914b5ce
Luis Marsiglia <luismarsiglia19@gmail.com>
2022-02-04 14:48:15 -0400

    slp endpoint will not show slpHistory without '?verbose=true' query

--- routes/player/slp.js
+++ routes/player/slp.js
@@ -4,9 +4,25 @@ const { compareObjects } = require('../../utils')
 const { fetchAccountData, addAccount } = require('../../utils/player')
 const Account = require('../../models/Account')
 
+const sendResult = (req, res, verbose, verbosedData) => {
+  if (verbose) {
+    return res.status(200).json(verbosedData)
+  } else {
+    const { slpHistory, _id, __v, ...data } = verbosedData._doc
+    return res.status(200).json(data)
+  }
+}
+
 router.get('/', async (req, res, next) => {
   try {
     let { address } = req.params
+    let { verbose } = req.query
+
+    // parsing verbose query as boolean
+    verbose = verbose === undefined ? false : verbose
+    verbose = verbose.toString().trim().toLowerCase()
+    verbose = verbose === 'true' || verbose === '1'
+
     address = address.replace('ronin:', '0x')
     const compactAddres = `0x...${address.substring(address.length - 4)}`
 
@@ -15,14 +31,13 @@ router.get('/', async (req, res, next) => {
     if (!dbData) {
       console.log(`Account ${compactAddres} not found. Saving...`)
       const savedAccount = await addAccount(address)
-      return res.status(200).json(savedAccount)
+      return sendResult(req, res, verbose, savedAccount)
     } else {
-      // dbData = dbData._doc
       const fetchedData = await fetchAccountData(address)
 
       if (compareObjects(dbData, fetchedData, ['totalSlp', 'claimableSlp', 'inGameSlp', 'lastClaimTime'])) {
         console.log(`Account ${compactAddres} found. No update needed`)
-        return res.status(200).json(dbData)
+        return sendResult(req, res, verbose, dbData)
       } else {
         const todaySoFar = fetchedData.inGameSlp - dbData.slpHistory.slice(-1)[0].amount
         const updatedData = {
@@ -33,7 +48,7 @@ router.get('/', async (req, res, next) => {
         }
 
         const updatedAccount = await Account.findOneAndUpdate({ address }, updatedData, { new: true })
-        return res.status(200).json(updatedAccount)
+        return sendResult(req, res, verbose, updatedAccount)
       }
     }
   } catch (error) {

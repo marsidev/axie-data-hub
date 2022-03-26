a225d6eb90dd5ec777c16a68b3fba711e24ae78c
Luis Marsiglia <luismarsiglia19@gmail.com>
2022-03-25 23:41:20 -0400

    cache only valid responses

--- middlewares/cache.js
+++ middlewares/cache.js
@@ -3,11 +3,12 @@ const cache = new NodeCache()
 
 module.exports = duration => (req, res, next) => {
   if (req.method !== 'GET') {
-    console.log('Not a GET request, passing control to next middleware')
+    // console.log('Not a GET request, passing control to next middleware')
     return next()
   }
 
   const key = `${req.method}-${req.originalUrl}`
+  // console.log(res.statusCode)
   const cached = cache.get(key)
   if (cached) {
     // console.log('Serving from cache')
@@ -17,7 +18,10 @@ module.exports = duration => (req, res, next) => {
   // console.log('Serving from server')
   res.sendResponse = res.json
   res.json = body => {
-    cache.set(key, body, duration)
+    if (!body.error && duration && duration > 0 && res.statusCode < 400) {
+      // console.log('Caching response')
+      cache.set(key, body, duration)
+    }
     res.sendResponse(body)
   }
   next()

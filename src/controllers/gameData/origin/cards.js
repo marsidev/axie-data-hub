5c25479438bda2bf48ca39922e8125e6ed4e67c9
Luis Marsiglia <luismarsiglia19@gmail.com>
2022-05-07 14:40:24 -0400

    added route /:patchId to get data of specific patch

--- src/controllers/gameData/origin/cards.js
+++ src/controllers/gameData/origin/cards.js
@@ -1,8 +1,10 @@
+/* eslint-disable no-useless-escape */
 const express = require('express')
 const router = express.Router()
 const cache = require('@middlewares/cache')
 const { formatCardsData } = require('@utils/origin/formatter')
 const filterObject = require('@utils/filterObject')
+const filesList = require('@utils/filesList')
 
 router.get('/', cache(600), async (req, res, next) => {
   const { query } = req
@@ -10,6 +12,36 @@ router.get('/', cache(600), async (req, res, next) => {
 
   try {
     const data = require('@assets/cards-data/origin/current/cards')
+
+    if (format === 'original') res.json(data)
+    else {
+      const formattedData = formatCardsData(data)
+      const filteredData = filterObject(formattedData, query)
+      res.json(filteredData)
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.get('/:patchId', cache(600), async (req, res, next) => {
+  const { query } = req
+  const { patchId } = req.params
+  const { format = 'default' } = query
+
+  const files = filesList('../assets/cards-data/origin')
+  const patchIds = files.filter(file => !file.includes('.'))
+  const availablePatches = patchIds.join(', ')
+
+  if (patchIds.indexOf(patchId) === -1) {
+    return res.status(400).json({
+      error: `PatchId \'${patchId}\' is not available. Available patches: ${availablePatches}. Check /cards/patches to see more about.`
+    })
+  }
+
+  try {
+    const data = require(`@assets/cards-data/origin/${patchId}/cards`)
+
     if (format === 'original') res.json(data)
     else {
       const formattedData = formatCardsData(data)
--- src/controllers/gameData/origin/tools.js
+++ src/controllers/gameData/origin/tools.js
@@ -1,7 +1,9 @@
+/* eslint-disable no-useless-escape */
 const express = require('express')
 const router = express.Router()
 const cache = require('@middlewares/cache')
 const { formatToolsData } = require('@utils/origin/formatter')
+const filesList = require('@utils/filesList')
 
 router.get('/', cache(600), async (req, res, next) => {
   const { query } = req
@@ -9,6 +11,33 @@ router.get('/', cache(600), async (req, res, next) => {
 
   try {
     const data = require('@assets/cards-data/origin/current/tools')
+    if (format === 'original') res.json(data)
+    else {
+      res.json(formatToolsData(data))
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.get('/:patchId', cache(600), async (req, res, next) => {
+  const { query } = req
+  const { patchId } = req.params
+  const { format = 'default' } = query
+
+  const files = filesList('../assets/cards-data/origin')
+  const patchIds = files.filter(file => !file.includes('.'))
+  const availablePatches = patchIds.join(', ')
+
+  if (patchIds.indexOf(patchId) === -1) {
+    return res.status(400).json({
+      error: `PatchId \'${patchId}\' is not available. Available patches: ${availablePatches}. Check /cards/patches to see more about.`
+    })
+  }
+
+  try {
+    const data = require(`@assets/cards-data/origin/${patchId}/tools`)
+
     if (format === 'original') res.json(data)
     else {
       res.json(formatToolsData(data))
